c975f59a525c691065929512ca38f0892c3d676c
Jessica Gladstone <jcg321@comcast.net>
2019-02-15 21:31:50 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,24 +1,20 @@
 class Ship
-    attr_accessor :name, :type, :booty
-    
-    @@all= []
-    
-    def initialize (name, type, booty)
-      @name = name
-      @type = type
-      @booty = booty
-      self.save
-    end
-    
-    def self.save
+    attr_reader :name, :type, :booty
+   
+    @@all = []
+   
+    def initialize(args)
+      @name = args[:name]
+      @type = args[:type]
+      @booty = args[:booty]
       @@all << self
     end
-    
+   
     def self.all
       @@all
     end
-    
+
     def self.clear
-      @@all = []
+        @@all = []
     end
-end
+  end
